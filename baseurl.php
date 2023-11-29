7ebd07912bec25da24d642add97fae2371da90f2
xxnirvana69xx <xxnirvana69xx@gmail.com>
2023-11-29 12:59:36 -0500

    Update baseurl.php

--- baseurl.php
+++ baseurl.php
@@ -4,6 +4,6 @@ function getBaseUrl() {
   if($host == "fpplocal") {
     return "http://host.docker.internal:8080/remotefalcon/api";
   }
-  return "http://192.168.200.107/remotefalcon/api";
+  return "http://192.168.200.107:8080/remotefalcon/api";
 }
 ?>
